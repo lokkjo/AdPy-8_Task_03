889a7f245d01d7b4fa7b734bbb37e09ae9a12654
Alexander Vinogradov <dra-katt@yandex.ru>
2019-10-20 16:43:24 +0300

    Исправление и упрощение кода

--- adv_print.py
+++ adv_print.py
@@ -1,17 +1,11 @@
 def adv_print(*args, **kwargs):
     source = str(args[0])
+    start = kwargs.get('start')
     if kwargs.get('start') is None:
         start = '\n'
-    else:
-        start = kwargs['start'] + '\n'
+    max_line = kwargs.get('max_line')
     if kwargs.get('max_line') is None:
         max_line = 0
-    else:
-        max_line = int(kwargs['max_line'])
-    if kwargs.get('in_file') is None:
-        in_file = None
-    else:
-        in_file = kwargs['in_file']
     def source_format(source): # на основе решения http://www.cyberforum.ru/python-beginners/thread1799717.html
         if max_line == 0:
             proxy_text = source
@@ -28,15 +22,15 @@ def adv_print(*args, **kwargs):
                     counter += 1
                 proxy_text += word
         return proxy_text
-    result = '' + start + source_format(source)
-    def output_file(in_file):
-        if in_file == None:
-            pass
-        else:
-            with open(in_file, 'wt', encoding='utf-8') as document:
-                document.write(result)
-    output_file(in_file)
+    result = '' + start + '\n' + source_format(source)
+    in_file = kwargs.get('in_file')
+    if in_file is None:
+        pass
+    else:
+        with open(in_file, 'wt', encoding='utf-8') as document:
+            document.write(result)
     print(result)
+
 
 if __name__ == '__main__':
     a = "Антиутопия (от «анти-» и «утопия»), также дистопия (" \
@@ -57,9 +51,8 @@ if __name__ == '__main__':
 
     adv_print('Задача 3\n')
     adv_print('Печать с указанием аргументов: \n')
-    adv_print(a, start='Тест adv_print', max_line=25,
+    adv_print(a, start='Тест adv_print', max_line=55,
               in_file='example.txt')
 
     adv_print('\nПечать без аргументов\n')
     adv_print(a)
-
